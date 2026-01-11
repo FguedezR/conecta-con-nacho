23b37edc5268f84b85c36bede746d3450615611a
Fernando <fernanguedezr@gmail.com>
2026-01-11 12:59:30 +0100

    chore: update TextReveal.tsx

--- src/components/RevealText.tsx
+++ src/components/RevealText.tsx
@@ -1,9 +1,13 @@
 import { motion } from "framer-motion";
 
-export const RevealText = ({ text, delay = 0 }) => {
+// Definimos qué tipos de datos recibe el componente
+interface Props {
+  text: string;     // Obligatorio: siempre será un texto
+  delay?: number;   // Opcional: si no se envía, será un número (ya lo inicializas a 0)
+}
+
+export const RevealText = ({ text, delay = 0 }: Props) => {
   return (
-    // Añadimos px-4 (espacio lateral) y py-2 (espacio vertical para las tildes o letras bajas)
-    // El -mx-4 compensa el px-4 para que el centrado sea perfecto
     <div className="overflow-hidden inline-block px-6 -mx-6 py-2">
       <motion.span
         initial={{ y: "105%", opacity: 0 }}
@@ -13,7 +17,7 @@ export const RevealText = ({ text, delay = 0 }) => {
           ease: [0.33, 1, 0.68, 1], 
           delay: delay 
         }}
-        className="inline-block whitespace-nowrap"
+        className="inline-block whitespace-nowrap font-zalando font-bold"
       >
         {text}
       </motion.span>
